196e1a2f8fda1f6afb18c6232ef4f8ffda693b0e
Max Igor <maxigor.ferreira@gmail.com>
2016-02-04 18:25:00 -0200

    copy and paste is not a design pattern

--- produtosWeb.js
+++ produtosWeb.js
@@ -1,9 +1,10 @@
 var http = require('http');
 
-var server = http.createServer(function(req,res){
-   res.end("<html><body><h1>Listando os produtos</h1></body></html>");
+var server = http.createServer(function(request,response){
+   if(request.url == "/produtos")
+   response.end("<html><body><h1>Listando os produtos</h1></body></html");
 });
 
-server.listen(3000);
+server.listen(3000,"localhost");
 
 console.log('servidor está rodando');
